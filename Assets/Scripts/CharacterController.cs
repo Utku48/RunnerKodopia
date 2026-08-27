9c3a96382e8d8d37aae5d7e74e1d1c0011a9e7f1
Utku Karakuyu <utku20600@gmail.com>
2026-08-27 12:24:21 +0300

    .

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private int speed;
+    [SerializeField] private float smooth_speed = 10f;
+
+    private float[] xPositions = { -0.33f, 0f, 0.33f };
+    private int currentPosIndex = 1;
+    Vector3 targetPos;
+
+
+
+    void Start()
+    {
+        targetPos = transform.position;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) && currentPosIndex > 0)
+        {
+            currentPosIndex--;
+            UpdateLateralPosition();
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && currentPosIndex < 2)
+        {
+            currentPosIndex++;
+            UpdateLateralPosition();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //ileri hareket yönü
+        Vector3 forwardMove = Vector3.forward * speed * Time.fixedDeltaTime;
+
+        //Hedef noktası pozisyonuna doğru yumuşak bir geçiş yap
+        Vector3 currentPosition = rb.position;
+        Vector3 letaralMove = Vector3.Lerp(currentPosition, targetPos, Time.fixedDeltaTime * smooth_speed);
+
+        //İleri ve yan haraketi birleşimi
+        Vector3 combineMove = new Vector3(letaralMove.x, transform.position.y, rb.position.z) + forwardMove;
+        rb.MovePosition(combineMove);
+    }
+
+    void UpdateLateralPosition()
+    {
+        targetPos = new Vector3(xPositions[currentPosIndex], transform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> roads = new List<GameObject>();
+    [SerializeField] private Transform player;
+    [SerializeField] Transform carSpawn;
+
+    private float previousPlayerZ;
+    [SerializeField] private int roadCount = 5;
+    [SerializeField] private float roadLength = 3.2f;
+
+    private float nextRoadZ;
+
+
+    void Start()
+    {
+
+        nextRoadZ = player.position.z;
+
+        for (int i = 0; i < roadCount; i++)
+        {
+            CreateRoad();
+        }
+    }
+
+    void Update()
+    {
+
+        if (player.position.z > nextRoadZ - (roadLength * roadCount))
+        {
+            CreateRoad();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Oyuncunun z pozisyonundaki değişimini hesapla
+        float deltaZ = player.transform.position.z - previousPlayerZ;
+
+        //Spawner pozisyonunu yukarıdaki değişim kadar değiştir
+        carSpawn.position += new Vector3(0, 0, deltaZ);
+
+        previousPlayerZ = player.position.z;
+    }
+
+    void CreateRoad()
+    {
+        Vector3 spawnPosition = new Vector3(0f, 0f, nextRoadZ);
+
+        GameObject selectedRoad = roads[Random.Range(0, roads.Count)];
+
+        Instantiate(selectedRoad, spawnPosition, Quaternion.identity);
+
+        nextRoadZ += roadLength;
+    }
+}
